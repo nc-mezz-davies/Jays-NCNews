a06f89e454c233a4b791ccea8973ffb11077c537
jaymulholland <jay.mulholland.7@gmail.com>
2025-02-28 14:09:27 +0000

    seed finalised

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # NC News Seeding
 
 - Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+
+
+-please run npm-install
+-please run npm run setup-dbs to create the necessary databases locally. 
+
+-inside setup-dbs.sql, make a note of the database names "nc_news" and "nc_news_test".
+-create two .env files, ".env.development" and ".env.test".
+these files should contain the text "PGDATABASE = " and the name of its database.
+
+-verify setup by running:
+-npm run test-seed
+-npm run seed-dev
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,163 @@
-const db = require("../connection")
+const db = require("../connection");
+const articles = require("../data/test-data/articles");
+const users = require("../data/test-data/users");
+const format = require("pg-format");
+const { convertTimestampToDate } = require("./utils");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db
+    .query("DROP TABLE IF EXISTS topics CASCADE") //<< write your first query in here.
+    .then(() => {
+      return createTopics();
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS users CASCADE");
+    })
+    .then(() => {
+      return createUsers();
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS articles CASCADE");
+    })
+    .then(() => {
+      return createArticles();
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS comments CASCADE");
+    })
+    .then(() => {
+      return createComments();
+    })
+    .then(() => {
+      return insertTopicData(topicData);
+    })
+    .then(() => {
+      return insertUserData(userData);
+    })
+    .then(() => {
+      return insertArticleData(articleData);
+    })
+    .then(() => {
+      return insertCommentData(commentData);
+    });
 };
 module.exports = seed;
+
+function createTopics() {
+  return db.query(`CREATE TABLE topics(
+    slug VARCHAR PRIMARY KEY, 
+    description VARCHAR , 
+    img_url VARCHAR(1000)  
+    );`);
+}
+
+function createUsers() {
+  return db.query(`CREATE TABLE users(
+    username VARCHAR PRIMARY KEY, 
+    name VARCHAR , 
+    avatar_url VARCHAR(1000)  
+    )`);
+}
+
+function createArticles() {
+  return db.query(`CREATE TABLE articles(
+    article_id SERIAL PRIMARY KEY, 
+    title VARCHAR , 
+    topic VARCHAR,
+    author VARCHAR ,
+    body TEXT ,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    votes INT ,
+    article_img_url VARCHAR(1000),
+       FOREIGN KEY (topic) REFERENCES topics(slug) ON DELETE CASCADE,
+       FOREIGN KEY (author) REFERENCES users(username) ON DELETE CASCADE
+
+    )`);
+}
+
+function createComments() {
+  return db.query(`CREATE TABLE comments(
+    comment_id SERIAL PRIMARY KEY NOT NULL, 
+    article_id INT, 
+    body TEXT ,
+    votes INT , 
+    author VARCHAR,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (article_id) REFERENCES articles(article_id) ON DELETE CASCADE,
+    FOREIGN KEY (author) REFERENCES users(username) ON DELETE CASCADE
+    
+    )`,
+   
+  );
+}
+
+function insertTopicData(data) {
+  const sql = format(
+    `INSERT INTO topics 
+    (slug, description, img_url) 
+    VALUES 
+    %L
+    RETURNING * 
+    `,
+    data.map((item) => [item.slug, item.description, item.img_url])
+  );
+  return db.query(sql);
+}
+
+function insertUserData(data) {
+  const sql = format(
+    `INSERT INTO users 
+    (username, name, avatar_url) 
+    VALUES 
+    %L
+    RETURNING * 
+    `,
+    data.map((item) => [item.username, item.name, item.avatar_url])
+  );
+  return db.query(sql);
+}
+
+function insertArticleData(data) {
+  //const date = convertTimestampToDate(data.created_at);
+  const sql = format(
+    `INSERT INTO articles 
+    ( title, topic, author, body, created_at, votes, article_img_url) 
+    VALUES 
+    %L
+    RETURNING * 
+    `,
+    data.map((item) => [
+      
+      item.title,
+      item.topic,
+      item.author,
+      item.body,
+      new Date(item.created_at),
+      item.votes,
+      item.article_img_url,
+    ]
+  
+  )
+  );
+  return db.query(sql);
+}
+
+function insertCommentData(data) {
+  const sql = format(
+    `INSERT INTO comments 
+    ( body, article_id, author, votes, created_at) 
+    VALUES 
+    %L
+    RETURNING * 
+    `,
+    data.map((item) => [
+      
+      item.body,
+      item.article_id,
+      item.author,
+      item.votes,
+      new Date(item.created_at),
+    ])
+  );
+  return db.query(sql);
+}
