d0181629fdfb27675490c5937f946345ed3bc35c
jaymulholland <jay.mulholland.7@gmail.com>
2025-03-11 11:35:48 +0000

    completed task 4

--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -2,8 +2,8 @@ const db = require('../db/connection');
 const seed = require('../db/seeds/seed');
 const data = require('../db/data/test-data/index');
 
-beforeAll(() => seed(data));
-afterAll(() => db.end());
+beforeEach(() => seed(data));
+afterEach(() => db.end());
 
 describe('seed', () => {
   describe('topics table', () => {
