cc97220f06c628f809237dfb04fc9a6eef7f17c1
jaymulholland <jay.mulholland.7@gmail.com>
2025-03-26 14:13:47 +0000

    8hosted

--- db/connection.js
+++ db/connection.js
@@ -3,18 +3,18 @@ const { Pool } = require("pg");
 
 
 const ENV = process.env.NODE_ENV || "development";
-
+require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
   
 const config = {};
 console.log(ENV, "<<");
-// if (ENV === "production") {
-//   config.connectionString = process.env.DATABASE_URL;
-//   config.max = 2;
-// }
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
-// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-//     throw new Error("PGDATABASE or DATABASE_URL not set");
-//   }
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set");
+  }
 
 module.exports = new Pool(config);
